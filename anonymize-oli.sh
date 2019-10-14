1a868be7ed533479028f6cd503e3c603bb94e96a
Martin van Velsen <vvelsen@gmail.com>
2019-10-14 11:17:08 -0400

    Reworked the anon script so that someone can start to using the tools right out of the box. This one can also be used as a template for other wrapping scripts.

--- anonymize-oli.sh
+++ anonymize-oli.sh
@@ -3,40 +3,42 @@ echo "Executing ..."
 
 outputFile=$2
 
-if [ -z "$1" ] ; then
+if [ -z "$1" ]
+  then
     echo "No input file supplied"
     exit 1
 fi
 
-if [ -z "$2" ] ; then
+if [ -z "$2" ]
+  then
     echo "No output file supplied, using input argument"
-    outputFile="{$1}.anonymized.tsv"
+    outputFile="$1.anonymized.tsv"
 fi
 
-echo "Writing to: {$outputFile}"
-echo $outputFile
-
-exit 1
+echo "Writing to: ${outputFile}"
 
 if type -p java; then
     echo found java executable in PATH
     _java=java
 elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-    echo found java executable in JAVA_HOME     
+    #echo found java executable in JAVA_HOME     
     _java="$JAVA_HOME/bin/java"
 else
     echo "no java"
+    exit 1
 fi
 
 if [[ "$_java" ]]; then
     version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-    echo version "$version"
+    #echo version "$version"
     if [[ "$version" > "1.5" ]]; then
-        echo version is more than 1.5
+        #echo version is more than 1.5
+        echo
     else         
-        echo version is less than 1.5
+        echo Java version is less than 1.5
+        exit 1
     fi
 fi
 
-java -cp ./dist/datafiltering-1.0-SNAPSHOT-jar-with-dependencies.jar edu.cmu.eberly.DataFiltering --operation hashcode --target 2 --iformat t --oformat t --input $1 --output outputFile
+java -cp ./dist/datafiltering-1.0-SNAPSHOT-jar-with-dependencies.jar edu.cmu.eberly.DataFiltering --cell_operation hashcode --target 2 --iformat t --oformat t --input $1 --output outputFile
 
